6bb5eb227afb368d5ca778781ca8528a80934b8d
mschubert <mschu.dev@gmail.com>
2014-08-10 15:28:47 +0100

    fix #17: add gcc-libs, R to LD_LIB_PATH to not let host libs screw up loading

--- etc/profile.d/ld_lib.sh
+++ etc/profile.d/ld_lib.sh
@@ -1,3 +1,3 @@
 #export LD_LIBRARY_PATH=$EPREFIX/usr/lib
-export LD_LIBRARY_PATH=$EPREFIX/usr/lib:$EPREFIX/usr/x86_64-pc-linux-gnu/lib/gcc
+export LD_LIBRARY_PATH=$EPREFIX/usr/lib:$EPREFIX/usr/x86_64-pc-linux-gnu/lib/gcc:$EPREFIX/usr/lib/R/lib
 
