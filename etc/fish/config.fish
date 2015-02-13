66e8aa24956114699329f47dad89fbd3d5f06925
mschubert <mschu.dev@gmail.com>
2015-02-13 18:04:31 +0000

    add LSF vars for fish (improves #26)

---
+++ etc/fish/config.fish
@@ -0,0 +1,44 @@
+#
+# Init file for fish
+#
+
+#
+# Some things should only be done for login terminals
+#
+
+if status --is-login
+
+    set LSF_BINDIR /ebi/lsf/ebi/9.1/linux2.6-glibc2.3-x86_64
+    set LSF_ENVDIR /ebi/lsf/ebi/conf
+    set LSF_LIBDIR /ebi/lsf/ebi/9.1/linux2.6-glibc2.3-x86_64/lib
+    set LSF_SERVERDIR /ebi/lsf/ebi/9.1/linux2.6-glibc2.3-x86_64/etc
+
+	#
+	# Set some value for LANG if nothing was set before, and this is a
+	# login shell.
+	#
+
+	if not set -q LANG >/dev/null
+		set -gx LANG en_US.UTF-8
+	end
+
+	# Check for i18n information in
+	# /etc/sysconfig/i18n
+
+	if test -f /etc/sysconfig/i18n
+		eval (cat /etc/sysconfig/i18n |sed -ne 's/^\([a-zA-Z]*\)=\(.*\)$/set -gx \1 \2;/p')
+	end
+
+	#
+	# Put linux consoles in unicode mode.
+	#
+
+	if test "$TERM" = linux
+		if expr "$LANG" : ".*\.[Uu][Tt][Ff].*" >/dev/null
+			if which unicode_start >/dev/null
+				unicode_start
+			end
+		end
+	end
+end
+
